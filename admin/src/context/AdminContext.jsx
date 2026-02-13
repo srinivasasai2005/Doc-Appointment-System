69a3f87f82eed614693ccc91fced197a647b86ea
Srinivasa Sai <srinivasasaibandi@gmail.com>
2026-02-13 14:26:20 +0530

    Update backend URL in AdminContext
    Replaced backend URL with a hardcoded value for the API endpoint.

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -13,7 +13,7 @@ const AdminContextProvider = (props) => {
     const [appointments, setAppointments] = useState([]);
     const [dashData, setDashData] = useState(false);
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const backendUrl = "https://doc-appointment-system-backend-bvn5.onrender.com";
 
     const getAllDoctors = async () => {
         try { 
